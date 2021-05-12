c009689db06a376f8b0909754c1f194df01c6329
eminkartci <emin.kartci@ozu.edu.tr>
2021-05-12 22:16:36 +0300

    Lecture 1

---
+++ AMeanSolver.java
@@ -0,0 +1,185 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+// Solver class
+public class AMeanSolver{
+
+    // MAIN METHOD
+    public static void main(String[] args){
+
+        ProblemData sampleData = ProblemData.readSampleOffers();
+        AMeanSolver solver = new AMeanSolver(sampleData);
+
+        solver.solveByRecursion(0, sampleData.areaCapacity);
+        // solver.presentSolution();
+        sampleData.showOffers();
+    }
+
+
+    // Attributes
+    private ProblemData problem;
+    private ArrayList<Offer> takenOffers = new ArrayList<Offer>();
+
+    // Constructor
+    public AMeanSolver(ProblemData problem){     
+        this.problem = problem;
+    }
+
+    // Present Solution
+    public void presentSolution(){
+
+        String content = "Academic license - for non-commercial use only - Expires 2021-06-20" + "\n"
+                        + "Optimize a model with " + this.problem.offers.size()  + " variables" + "\n"
+                        + "Model fingerprint: " + this.hashCode() + "\n\n";
+
+        content += "\n Taken Offers: \n\n";
+
+        for(Offer o : takenOffers){
+            content += o.toString();
+        }
+
+        System.out.println(content);
+
+
+    }
+
+    // SOLUTION METHOD 1
+    public double solveByRecursion(int nextIndex, double freeArea){
+
+        Offer currentOffer = problem.offers.get(nextIndex);
+        // System.out.println("Next Index: " + nextIndex + " MAX: " + (problem.offers.size() -1));
+        // System.out.println("Free area : " + freeArea);
+        // Check if this is last item
+        if(nextIndex == problem.offers.size() -1){
+
+            // If There is enough area
+            if (freeArea >= currentOffer.getArea()){
+
+                // Take
+                takenOffers.add(currentOffer);
+                return currentOffer.getProfit();
+
+            }else{ // Otherwise
+
+                // Don't Take
+                return 0;
+
+            } 
+
+        }// Otherwise Middle Process
+        else{
+
+            // If there is not enogh area
+            if (freeArea < currentOffer.getArea()){
+                // Don't take and go next
+                return 0 + solveByRecursion(nextIndex + 1, freeArea);
+            }else{ // Otherwise
+ 
+                // There are 2 options
+
+                    // Take it
+                double takeOption  = currentOffer.getProfit() + solveByRecursion(nextIndex + 1, freeArea - currentOffer.getArea());
+                    // Leave it
+                double leaveOption = 0 + solveByRecursion(nextIndex + 1, freeArea);
+                // Return Which one has greater profit
+                if( takeOption > leaveOption){
+
+                    takenOffers.add(currentOffer);
+                    return takeOption;
+
+                }else{
+
+                    return leaveOption;
+
+                }
+
+            }
+        }
+    }
+
+}
+
+class ProblemData{
+
+    // Constants
+    private static int SAMPLE_COUNT = 10;
+    private static int SAMPLE_AREA_MIN_LIMIT = 200;
+    private static int SAMPLE_AREA_MAX_LIMIT = 500;
+    private static int SAMPLE_PROFIT_MIN_LIMIT = 200;
+    private static int SAMPLE_PROFIT_MAX_LIMIT = 500;
+    private static int SAMPLE_CAPACITY_MIN_LIMIT = 400;
+    private static int SAMPLE_CAPACITY_MAX_LIMIT = 2000;
+
+    // Attributes
+    public ArrayList<Offer> offers;
+    public double areaCapacity;
+    private static Random random = new Random();
+
+    public static ProblemData readSampleOffers(){
+
+        ProblemData pd = new ProblemData();
+        ArrayList<Offer> tempOffers = new ArrayList<Offer>();
+
+        for(int i = 0 ; i < SAMPLE_COUNT ; i++){
+
+            Offer tempOffer = new Offer("Offer_"+(i+1), SAMPLE_AREA_MIN_LIMIT + random.nextInt(SAMPLE_AREA_MAX_LIMIT - SAMPLE_AREA_MIN_LIMIT) , SAMPLE_PROFIT_MIN_LIMIT + random.nextInt(SAMPLE_PROFIT_MAX_LIMIT - SAMPLE_PROFIT_MIN_LIMIT));
+            tempOffers.add(tempOffer);
+
+        }
+
+        pd.offers = tempOffers;
+        pd.areaCapacity = SAMPLE_CAPACITY_MIN_LIMIT + random.nextInt(SAMPLE_CAPACITY_MAX_LIMIT - SAMPLE_CAPACITY_MIN_LIMIT);
+        
+        return pd;
+    }
+
+    public void showOffers(){
+        for (Offer o : offers){
+            System.out.println(o);
+        }
+    }
+
+}
+
+
+// Offer Class
+class Offer{
+
+    // Attribute
+    private String offerName;
+    private double area;
+    private double profit;
+
+    // Constructor
+    public Offer(String name,double area, double profit){
+
+        this.offerName = name;
+        this.area = area;
+        this.profit = profit;
+
+    }
+
+    // Getter Setter
+    public String getOfferName(){
+        return this.offerName;
+    }
+
+    public String toString(){
+        return "\n--- Offer ---" + "\n"
+            + "Offer Name: " + this.offerName + "\n"
+            + "Area      : " + this.area      + "\n"
+            + "Profit    : " + this.profit    + "\n";
+    }
+
+    public double getArea(){
+        return this.area;
+    }
+
+    public double getProfit(){
+        return this.profit;
+    }
+
+        
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ShoppingCenterOptimization
 This project selects best rent offers for customers.
+
+There will be some offers. The shopping center has area limit. The objective is to accept best set for offers with greatest profit without exceeding the are limit !
+
+### Offer Class
+
+#### Attributes
+1. Offer
+2. Area
+
